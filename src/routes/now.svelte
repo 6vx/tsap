451297e0a3462ea8abf006b2638c7b0f69d3d605
Timothy Godin <support@kitebot.ca>
2020-09-27 21:34:34 -0600

    something

---
+++ src/routes/now.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<div>
+    <h1>Nownownow</h1>
+    <p>I'm in Calgary playing with a doggo.</p>
+</div>
+
+<style>
+    h1 {
+        font-size: large;
+    }
+</style>
