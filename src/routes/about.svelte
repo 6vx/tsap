9a0b92c44663c65c4cfd37169dc786126b2b521e
Timothy Godin <support@kitebot.ca>
2020-10-01 16:16:20 -0600

    Helo

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,23 +4,11 @@
     <h3>Why I think how I think</h3>
     
     <ul>
-        <li><a href="http://derrenbrown.co.uk/">Derren Brown</a></li>
-        <li><a href="https://lostpedia.fandom.com/wiki/John_Locke">John Locke</a></li>
-        <li><a href="https://sive.rs/">Derek Sivers</a></li>
-        <li><a href="https://samharris.org/">Sam Harris</a></li>
-        <li><a href="https://elderscrolls.fandom.com/wiki/Vivec">Saint Vivec</a></li>
-        <li><a href="https://tim.blog/">Tim Ferriss</a></li>
+        <li><a target="_blank" href="http://derrenbrown.co.uk/">Derren Brown</a></li>
+        <li><a target="_blank" href="https://lostpedia.fandom.com/wiki/John_Locke">John Locke</a></li>
+        <li><a target="_blank" href="https://sive.rs/">Derek Sivers</a></li>
+        <li><a target="_blank" href="https://samharris.org/">Sam Harris</a></li>
+        <li><a target="_blank" href="https://elderscrolls.fandom.com/wiki/Vivec">Saint Vivec</a></li>
+        <li><a target="_blank" href="https://tim.blog/">Tim Ferriss</a></li>
     </ul>
-
-    <h3>What I endeavour to accomplish</h3>
-    
-    <ul>
-        <li>Never stop moving</li>
-        <li>Solving puzzles that help people</li>
-        <li>Make life easier for others</li>
-    </ul>
-
-    
-
-
 </div>
