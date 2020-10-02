73dc3b499711bbe0c6a447f69a40bba2699fd1a6
Timothy Godin <support@kitebot.ca>
2020-10-01 19:49:52 -0600

    adjust

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,11 +4,11 @@
     <h3>Why I think how I think</h3>
     
     <ul>
-        <li><a target="_blank" href="http://derrenbrown.co.uk/">Derren Brown</a></li>
-        <li><a target="_blank" href="https://lostpedia.fandom.com/wiki/John_Locke">John Locke</a></li>
-        <li><a target="_blank" href="https://sive.rs/">Derek Sivers</a></li>
-        <li><a target="_blank" href="https://samharris.org/">Sam Harris</a></li>
-        <li><a target="_blank" href="https://elderscrolls.fandom.com/wiki/Vivec">Saint Vivec</a></li>
-        <li><a target="_blank" href="https://tim.blog/">Tim Ferriss</a></li>
+        <li><a target="_blank" href="http://derrenbrown.co.uk/">Derren Brown</a> - Skepticism and more. Question what is influencing you.</li>
+        <li><a target="_blank" href="https://lostpedia.fandom.com/wiki/John_Locke">John Locke</a> - Passion. Choosing what to believe in. It's never <i>been</i> easy.</li>
+        <li><a target="_blank" href="https://sive.rs/">Derek Sivers</a> - The opposite is also true. Do not fall into the trap of thinking you know something.</li>
+        <li><a target="_blank" href="https://samharris.org/">Sam Harris</a> - Have difficult discussions. Truth is more important than anything.</li>
+        <li><a target="_blank" href="https://elderscrolls.fandom.com/wiki/Vivec">Vivec</a> - Embody strength. Project loving-kindness. An iron hand in a velvet glove.</li>
+        <li><a target="_blank" href="https://tim.blog/">Tim Ferriss</a> - Prioritize based on your fears, not your desires.</li>
     </ul>
 </div>
