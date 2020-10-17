6dc67738014598839c1e2ffa5102a4a38c6343d1
Timothy Godin <support@kitebot.ca>
2020-10-17 00:24:00 -0600

    This is smart.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <div class="homepage">
 	<div class="infoline">
-        <div class="links"><span><a href="/about">about</a></span></div>
+        <!-- <div class="links"><span><a href="/about">about</a></span></div> -->
 		<div class="links"><span><a href="/portfolio">portfolio</a></span></div>
 		<div class="links"><span><a href="/resume">resume</a></span></div>
         <div class="links"><span><a href="/contact">contact</a></span></div>
@@ -12,11 +12,11 @@
 	<p>Generally keep to myself, but I delight in playing devil's advocate to test ideas. I'm interested in what makes others tick.</p>
 	<p>Born and raised in Canada. Mostly lived in Ontario, then BC, and currently in Alberta.</p>
 	
-	<h2>What am I doing now?</h2>
+	<!-- <h2>What am I doing now?</h2>
 	<p>Check out my <a href="/now">"now"</a> page.</p>
 	
 	<h2>More?</h2>
-	<p>Head over to <a href="/about">the about page</a> if you need more (creeper).</p>
+	<p>Head over to <a href="/about">the about page</a> if you need more (creeper).</p> -->
 
 	<h2>Contact</h2>
 	<p>There are a few ways <a href="/contact">here.</a></p>
